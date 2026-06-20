8eced99cdbb04c15e96198e9eb87f033821e9d41
raydanielg <airezra2+1@gmail.com>
2026-06-20 04:09:01 +0300

    commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -82,7 +82,7 @@ class RegisterController extends Controller
      * @param  mixed  $user
      * @return mixed
      */
-    protected function registered($request, $user)
+    protected function registered(Request $request, $user)
     {
         return redirect($this->redirectPath());
     }
