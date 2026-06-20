321423fa7ad9991eba166044d49fbb856223408d
raydanielg <airezra2+1@gmail.com>
2026-06-20 17:02:12 +0300

    commit

--- resources/views/components/receipt.blade.php
+++ resources/views/components/receipt.blade.php
@@ -7,7 +7,6 @@
                     <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
                 </div>
                 <div>
-                    <p class="text-[10px] text-gray-400 uppercase tracking-widest font-semibold">Official Receipt</p>
                     <h2 class="text-lg font-extrabold text-gray-900 tracking-tight">SalamaPay</h2>
                 </div>
             </div>
