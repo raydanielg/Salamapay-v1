7c7fb31e12cf568412b9f1a4e39603589a084137
raydanielg <airezra2+1@gmail.com>
2026-06-20 03:49:23 +0300

    commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,24 +13,28 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin SalamaPay',
-            'first_name' => 'Admin',
-            'last_name' => 'SalamaPay',
-            'email' => 'admin@salamapay.com',
-            'phone' => '255712345678',
-            'role' => 'admin',
-            'password' => Hash::make('Admin@1234'),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@salamapay.com'],
+            [
+                'name' => 'Admin SalamaPay',
+                'first_name' => 'Admin',
+                'last_name' => 'SalamaPay',
+                'phone' => '255712345678',
+                'role' => 'admin',
+                'password' => Hash::make('Admin@1234'),
+            ]
+        );
 
-        User::create([
-            'name' => 'Test User',
-            'first_name' => 'Test',
-            'last_name' => 'User',
-            'email' => 'user@salamapay.com',
-            'phone' => '255712345679',
-            'role' => 'user',
-            'password' => Hash::make('User@1234'),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user@salamapay.com'],
+            [
+                'name' => 'Test User',
+                'first_name' => 'Test',
+                'last_name' => 'User',
+                'phone' => '255712345679',
+                'role' => 'user',
+                'password' => Hash::make('User@1234'),
+            ]
+        );
     }
 }
