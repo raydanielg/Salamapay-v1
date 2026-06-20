6487d9e05f87c3d09e448e2bd046a7b9b22bd0ed
raydanielg <airezra2+1@gmail.com>
2026-06-20 13:20:18 +0300

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
